732c129f49bdd508531b5b8abae09f4d45af150e
Sihad Begovic <sihadbegovic@gmail.com>
2023-11-03 08:40:47 +0100

    Fix the issuechecking out the project by AppCenter CI/CD tool

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Instabug",
-            url: "https://github.com/Instabug/Instabug-SP/releases/download/12.2.0/Instabug-XCFramework.zip",
+            path: "Instabug-12.2.0.zip",
             checksum: "5a7a8e350736e91e8d8d37753870092eff33caa5415f27359729594aae1581fd")
     ]
 )
